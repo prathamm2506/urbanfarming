35d0dba2c265274a58c7662c0f61d6cb4dc22665
Sarahaha12 <sarahshaikh70211@gmail.com>
2024-09-20 11:40:18 +0530

    basic filter added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Courses from './components/courses/Courses'
 import Home from './components/home/Home'
 import Us from './components/us/Us'
+import Coursedesc from './components/courses/Course/Coursedesc'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <Home/>
     <Us/>
      <Courses/>
+     <Coursedesc/>
     </>
   )
 }
