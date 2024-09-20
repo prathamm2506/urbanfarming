4129085784d210e10d07ade759c06f1d578aec97
UdayanKundu222 <149853824+UdayanKundu222@users.noreply.github.com>
2024-09-20 11:47:52 +0530

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Courses from './components/courses/Courses'
 import Home from './components/home/Home'
 import Us from './components/us/Us'
+import Marketplace from './components/Marketplace/Marketplace'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <Home/>
     <Us/>
      <Courses/>
+     <Marketplace/>
     </>
   )
 }
---
+++ src/components/Marketplace/Marketplace.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import './Marketplace.css';
+import img1 from '../../assets/basil.webp';
+import img2 from '../../assets/bean.webp';
+import img3 from '../../assets/capsicum.webp';
+import img4 from '../../assets/chill.webp';
+import img5 from '../../assets/chrysanthemum.webp';
+import img6 from '../../assets/coriander.webp';
+import img7 from '../../assets/cucumber.webp';
+import img8 from '../../assets/marigold.webp';
+import img9 from '../../assets/spinach.webp';
+import img10 from '../../assets/support.webp';
+import img11 from '../../assets/tomato.webp';
+import img12 from '../../assets/tray.webp';
+
+const Marketplace = () => {
+  return (
+    <div>
+      <main className="main-content">
+        <h1>Featured Products</h1>
+        <div className="product-grid">
+          <div className="product-card">
+            <img class="img" src={img1} alt="Basil" />
+            <h2>Basil Seeds</h2>
+            <p>₹50</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+          
+          <div className="product-card">
+            <img  class="img" src={img3} alt="Capsicum" />
+            <h2>Capsicum Seeds</h2>
+            <p>₹50</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+          <div className="product-card">
+            <img  class="img" src={img4} alt="Chilli" />
+            <h2>Green Chilli Seeds</h2>
+            <p>₹50</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+          <div className="product-card">
+            <img class="img" src={img5} alt="chrysanthemum" />
+            <h2>Chrysanthemum Seeds</h2>
+            <p>₹50</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+          <div className="product-card">
+            <img  class="img" src={img6} alt="Coriander" />
+            <h2>Coriander Seeds</h2>
+            <p>₹50</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+          <div className="product-card">
+            <img  class="img" src={img7} alt="Cucumber" />
+            <h2>Cucumber Seeds</h2>
+            <p>₹50</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+          <div className="product-card">
+            <img  class="img" src={img8} alt="Marigold" />
+            <h2>Marigold Seeds</h2>
+            <p>₹50</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+          <div className="product-card">
+            <img  class="img" src={img9} alt="Spinach" />
+            <h2>Spinach Seeds</h2>
+            <p>₹50</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+          <div className="product-card">
+            <img  class="img" src={img10} alt="Support" />
+            <h2>Plant support</h2>
+            <p>₹550</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+          <div className="product-card">
+            <img  class="img" src={img11} alt="Tomato" />
+            <h2>Tomato Seeds</h2>
+            <p>₹50</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+          <div className="product-card">
+            <img  class="img" src={img12} alt="Trayr" />
+            <h2>Seedling Tray</h2>
+            <p>₹250</p>
+            <button className="btn">
+              <i className="fas fa-shopping-cart"></i> Buy Now
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Marketplace;
