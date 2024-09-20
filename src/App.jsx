c8a87dab6f2a68121322694aabce41c76425bb29
prathamm2506 <fs20if005@gmail.com>
2024-09-20 11:42:12 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import Courses from './components/courses/Courses'
 import Home from './components/home/Home'
 import Us from './components/us/Us'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Navbar/>
     <Home/>
     <Us/>
      <Courses/>
